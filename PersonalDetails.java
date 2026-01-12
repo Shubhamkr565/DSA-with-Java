ec3de52fd7dfbf73c57faafa0584fdfe5215a0c3
Shubham <shubhamkr565@gmail.com>
2026-01-13 00:37:46 +0530

    Removed .class files and added gitignore

--- PersonalDetails.java
+++
@@ -1,7 +0,0 @@
-public class PersonalDetails{
-    public static void main(String[] args){
-        System.out.println("Name: Shubham");
-        System.out.println("Age: 25");
-        System.out.println("City: Chapra");
-    }
-}
